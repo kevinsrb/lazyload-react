418a5e14f025e4a6c547e54ac6d4e793f0efcbb7
KEVINSRB <kevinsrb.1999@gmail.com>
2023-03-03 15:41:24 -0500

    rutas anidades lista

---
+++ src/routes/routes.ts
@@ -0,0 +1,32 @@
+import { lazy, LazyExoticComponent } from "react";
+import { NoLazy } from "../01-lazyload/pages/NoLazy";
+// import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
+
+type JSXComponent = () => JSX.Element
+
+interface Route {
+    to: string;
+    path: string;
+    Component: LazyExoticComponent<JSXComponent> | JSXComponent;
+    name: string;
+}
+
+const LazyLayout = lazy(() => import(/* webpackChunkName: "LazyLayout" */ '../01-lazyload/layout/LazyLayout'))
+const Lazy1 = lazy(() => import(/* webpackChunkName: "LazyPage1" */ '../01-lazyload/pages/LazyPage1'))
+const Lazy2 = lazy(() => import(/* webpackChunkName: "LazyPage2" */ '../01-lazyload/pages/LazyPage2'))
+const Lazy3 = lazy(() => import(/* webpackChunkName: "LazyPage3" */ '../01-lazyload/pages/LazyPage3'))
+
+export const routes: Route[] = [
+    {
+        path: '/lazyload/*',
+        to: '/lazyload',
+        Component: LazyLayout, 
+        name: 'LazyLayout- Das'
+    },
+    { 
+        to: '/no-lazy',
+        path: 'no-lazy',
+        Component: NoLazy,
+        name: 'no-lazy'
+    }
+]
